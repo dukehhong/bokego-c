2c021b63b8614750ea31f868fc306a8d6b536150
dukehhong <dukehhong@yahoo.com>
2020-11-23 13:35:05 -0800

    play against bokeGo

--- src/app.cpp
+++ src/app.cpp
@@ -5,50 +5,56 @@
 #include "policy_net.h"
 #include "torch/script.h"
 #include "data_loader.h"
-
+#include "mcts.h"
+#include<vector>
 #include<fstream>
-typedef std::tuple<std::string, std::string, int, std::string> sample;
-
-void makeFeatues(DataLoader &dl, PolicyNet &model, std::vector<std::string>* lines, int i, int j){
-  Board game = Board(9);
-  for(int k=i; k<j; k++){
-    sample attr = dl.makeSample((*lines)[k]);
-    game.loadState(std::get<0>(attr), std::get<2>(attr), std::get<1>(attr));
-    model.features(game);
-  }
-}
+#include<random>
 
 int main(int argc, const char* argv[]) {
-  DataLoader dl = DataLoader();
-  dl.mount("../data/boards_aug.csv");
-  std::cout << "Loading data..." << std::endl;
-  dl.process(0, 10000);
-  std::cout << "Finished loading data..." << std::endl;
-  auto s = dl.getSamples();
+  if (argc != 2) {
+    std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+    return -1;
+  }
+
+
+  torch::jit::script::Module module;
+  try {
+    module = torch::jit::load(argv[1]);
+  }
+  catch (const c10::Error& e) {
+    std::cerr << "error loading the model\n";
+    return -1;
+  }
 
   Board game = Board(9);
   PolicyNet model = PolicyNet();
 
-  int num_workers = 10;
-  int partition = (*s).size()/num_workers;
-  std::vector<std::thread> threads;
+  MCTS ts = MCTS(20, 1.0, module);
+  ts._root->_lastmove = -1;
+  ts._root->_turn = 0;
 
-  for(int i = 0; i<num_workers; i++){
-    threads.push_back(std::thread(makeFeatues, std::ref(dl), std::ref(model), s, i*partition, (i+1)*partition));
-  }
-
-  std::cout << "Making features..." << std::endl;
-  auto start = std::chrono::high_resolution_clock::now();
-
-  for(auto &thread : threads){
-    thread.join();
-  }
-
-  auto end = std::chrono::high_resolution_clock::now();
-  std::cout << "Finished making features..." << std::endl;
-
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-  std::cout << "Time: " << duration.count()*0.000001 << " seconds" << std::endl;
   
+  //auto start = std::chrono::high_resolution_clock::now();
+  int turn = 0;
+  while(true){
+    int i, j;
+    std::cin >> i >> j;
+
+    ts.play(ts._root, 9*(i-1)+j-1);
+    ts._root->print();
+    if(turn <= 10){
+      ts.rollout(ts._root, 20);
+    }else{
+      ts.rollout(ts._root, 1000);
+    }
+    turn++;
+    ts.choose();
+    ts._root->print();
+  }
+  //auto end = std::chrono::high_resolution_clock::now();
+
+  //auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+  //std::cout << "Time taken: " << duration.count() << std::endl;
+
   return 0;
 }
